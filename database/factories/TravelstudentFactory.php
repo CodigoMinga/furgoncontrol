bda9b7f87196ad8fa5dc5f11ca9d852cf7d32caa
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-02-26 19:21:13 -0300

    Carga archivos a la repo

---
+++ database/factories/TravelstudentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Travelstudent;
+use Faker\Generator as Faker;
+
+$factory->define(Travelstudent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
