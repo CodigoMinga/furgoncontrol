c4a93499d86d266f5603c692d91cc0d978d8e2ed
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-02-28 21:36:35 -0300

    Add resset password route

---
+++ resources/views/generic.blade.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Furgoncontrolado.cl</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta property="og:site_name" content="www.furgoncontrolado.cl">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{ url('/') }}/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ url('/') }}/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{ url('/') }}/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ url('/') }}/dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ url('/') }}/plugins/iCheck/square/blue.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <style>
+        #grad {
+            background-image: linear-gradient(to right, red , yellow);
+            border-radius:20px;
+            width:100%;
+        }
+
+        #grad2 {
+            background: rgb(255,255,255);
+            background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,158,0,1) 100%);
+            max-height: 80%;
+        }
+    </style>
+</head>
+<body class="hold-transition login-page" id="grad2">
+<div class="login-box">
+    <div class="login-logo">
+    <!--   <img src="{{url("/")}}/images/logo_2021.png" width="100%"> -->
+
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body" style="border-radius: 10px">
+      <b>{{$message}}</b>
+
+    <a href="{{url('/app/login')}}" type="submit" class="btn btn-primary btn-block btn-flat">Volver al Login</a>
+
+
+
+
+
+
+    </div>
+    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="{{ url('/') }}/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ url('/') }}/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="{{ url('/') }}/plugins/iCheck/icheck.min.js"></script>
+<script>
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' /* optional */
+        });
+    });
+</script>
+</body>
+</html>
