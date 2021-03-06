7d7643c187edc15bd5abc504c3eb0c656910f197
Roberto Sierra <roberto30589@gmail.com>
2021-03-06 09:32:47 -0300

    termómetro con estilo

--- resources/views/travel/addassistance.blade.php
+++ resources/views/travel/addassistance.blade.php
@@ -2,14 +2,99 @@
 @extends('template.phonecontainer')
 
 <style>
-    input[type=range][orient=vertical]
+    input[type="range"].range
     {
-        writing-mode: bt-lr; /* IE */
-        -webkit-appearance: slider-vertical; /* WebKit */
-        width: 8px;
-        height: 200px;
-        padding: 0 5px;
+        cursor: pointer;
+        width: 100% !important;
+        -webkit-appearance: none;
+        z-index: 200;
+        height: 50px;
+        border: 3px solid white;
+        background-color: rgba(0,0,0,0);
+        border-radius: 4rem;
+        z-index:50;
+        outline: none;
     }
+    
+    input[type="range"].range::-webkit-slider-thumb, input[type="range"].range::-moz-range-thumb
+    {
+        -webkit-appearance: none;
+        width: 44px;
+        height: 44px;
+        background-color: white;
+        border-radius: 50%;
+        position: relative;
+        z-index:50;
+    }
+
+    #barra
+    {
+        display: block;
+        position: absolute;
+        background-color: #d50000;
+        height: 44px;
+        width:100%;
+        top:3px;
+        border-radius: 4rem;
+        left:0px;
+        z-index:1;  
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+        pointer-events: none;
+    }
+    #bulbo{
+        display: block;
+        position: absolute;
+        background-color: #d50000;
+        width:  80px;
+        height: 80px;
+        border-radius: 50%;
+        border: 3px solid white;
+        top:-16px;
+        left:-40px;
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+        pointer-events: none;
+    }
+    .medida{
+        display: block;
+        position: absolute;
+        width: 0px;
+        height: 22px;
+        left:0px;
+        border-style: solid;
+        border-width: 0px 2px 0 0;
+        z-index:55;  
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+        pointer-events: none;
+        float: left;
+    }
+    
+/*
+    input[type="range"].range
+    {
+        -webkit-transform:rotate(270deg);
+        -moz-transform:rotate(270deg);
+        -o-transform:rotate(270deg);
+        -ms-transform:rotate(270deg);
+        transform:rotate(270deg);
+    }
+*/
+
+
     .modal-dialog-centered{
         margin-top:25vh!important;
     }
@@ -65,8 +150,23 @@
                     <label for="exampleInputEmail1">Temperatura</label>
                     <input required type="number" step="0.1" min="32" max="42" name="temperature" id="temp" placeholder="" value="{{isset($travelstudent) ? $travelstudent->temperature : 35}}">
                 </div>
-
-                <input type="range" step="0.1" min="32" max="42" value="{{isset($travelstudent) ? $travelstudent->temperature : 35}}" id="slider" orient="vertical">
+                <div style="position:relative;margin-top:3rem;margin-left:3rem;margin-bottom:1rem;">
+                    <div id="bulbo"></div>
+                    <div style="position:relative;margin-left:44px;">
+                        <div class="medida"></div>
+                        <div class="medida" style="left:10%"></div>
+                        <div class="medida" style="left:20%"></div>
+                        <div class="medida" style="left:30%"></div>
+                        <div class="medida" style="left:40%"></div>
+                        <div class="medida" style="left:50%"></div>
+                        <div class="medida" style="left:60%"></div>
+                        <div class="medida" style="left:70%"></div>
+                        <div class="medida" style="left:80%"></div>
+                        <div class="medida" style="left:90%"></div>
+                    </div>
+                    <input class="range" type="range" step="0.1" min="32" max="42" value="{{isset($travelstudent) ? $travelstudent->temperature : 35}}" id="slider">
+                    <div id="barra"></div>
+                </div>
             </div>
         </div>              
         <br>
@@ -107,13 +207,25 @@
     <script>
         var slider = document.getElementById("slider");
         var temp = document.getElementById("temp");
+        var barra = document.getElementById("barra");
         
         slider.oninput = function() {
             temp.value=this.value;
+            sliderbar();
         }
 
         temp.onfocusout = function() {
             slider.value=this.value;
+            sliderbar();
+        }
+        sliderbar();
+
+        function sliderbar(){
+            var value=slider.value
+            var width = slider.offsetWidth;
+            var x = (10*value-320)/100;
+            var calc = x*width+(50-50*x);
+            barra.style.width=calc+"px";
         }
     </script>
 @stop
